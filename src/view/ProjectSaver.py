cdb410e204b72f75210dd4bf7a733dec60abb210
Corqi <72660203+Corqi@users.noreply.github.com>
2022-05-24 22:35:13 +0200

    Update ProjectSaver.py

--- src/view/ProjectSaver.py
+++ src/view/ProjectSaver.py
@@ -2,19 +2,22 @@ import pickle
 
 
 class ProjectSaver:
-    def __init__(self, project):
-        with open("project.p", "wb") as f:
+    def init(self, project):
+        with open("../project.p", "wb") as f:
             pickle.dump(project, f, pickle.HIGHEST_PROTOCOL)
 
 
 class Project:
-    def __init__(self, board_data=None, table_data=None):
+    def init(self, board_data=None, table_data=None):
         self.board_data = board_data
         self.table_data = table_data
 
 
 class ProjectReader:
-    def __init__(self):
-        with open("project.p", "rb") as f:
-            self.project = pickle.load(f)
-
+    def init(self):
+        try:
+            with open("../project.p", "rb") as f:
+                self.project = pickle.load(f)
+        except FileNotFoundError:
+            self.project = None
+            print("Project file not found")
